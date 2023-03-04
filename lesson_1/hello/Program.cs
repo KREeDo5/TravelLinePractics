3e0feb6f042171d9d33df15b416a3511030a8920
Dmitry <mitya.lyashko.02@mail.ru>
2023-03-04 21:47:04 +0300

    first practice_lesson

---
+++ lesson_1/hello/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace part
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            First first = new First("hi");
+            first.Name = "Hello";
+
+            Console.WriteLine(first.Name);
+        }
+    }
+}
---
+++ lesson_1/hello/write.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part
+{
+    internal class First
+    {
+        private string _name;
+
+        public string Name
+        {
+            get 
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public First(string name)
+        {
+            _name = name;
+        }
+    }
+}
